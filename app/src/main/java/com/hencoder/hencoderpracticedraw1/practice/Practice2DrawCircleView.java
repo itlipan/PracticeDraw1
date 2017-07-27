0f469dcaa1bfccd705713852b7d549a8bb161517
Paullee <javaclee@foxmail.com>
2017-07-27 17:07:27 +0800

    practice 1-3

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice2DrawCircleView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice2DrawCircleView.java
@@ -2,6 +2,8 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -10,15 +12,25 @@ public class Practice2DrawCircleView extends View {
 
     public Practice2DrawCircleView(Context context) {
         super(context);
+        init();
     }
 
     public Practice2DrawCircleView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public Practice2DrawCircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
     }
+
+    Paint mPaint = new Paint();
+
+    private void init() {
+
+    }
+
 
     @Override
     protected void onDraw(Canvas canvas) {
@@ -26,5 +38,18 @@ public class Practice2DrawCircleView extends View {
 
 //        练习内容：使用 canvas.drawCircle() 方法画圆
 //        一共四个圆：1.实心圆 2.空心圆 3.蓝色实心圆 4.线宽为 20 的空心圆
+        mPaint.setAntiAlias(true);
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setColor(Color.BLACK);
+        canvas.drawCircle(getWidth() / 4, getHeight() / 4, 200, mPaint);
+        mPaint.setStyle(Paint.Style.STROKE);
+        canvas.drawCircle(getWidth() * 3 / 4, getHeight() / 4, 200, mPaint);
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setColor(Color.BLUE);
+        canvas.drawCircle(getWidth() / 4, getHeight() * 3 / 4, 200, mPaint);
+        mPaint.setColor(Color.BLACK);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(20);
+        canvas.drawCircle(getWidth() * 3 / 4, getHeight() * 3 / 4, 200, mPaint);
     }
 }
